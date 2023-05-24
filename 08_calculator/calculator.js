517662b098149e21c7c7875470aa1626dd0fed30
humayunrasheed <humayun.51959@gmial.com>
2023-05-24 19:48:47 +0530

    8 exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum =0;
+  for ( let i=0;i<array.length;i++){
+    sum+=array[i];
+  }
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mul =1;
+  for ( let i=0;i<array.length;i++){
+    mul*=array[i];
+  }
+	return mul;
 };
 
-const power = function() {
-	
+const power = function(num,val) {
+  let ans=1;
+  for (let i=0;i<val;i++){
+    ans*=num;
+  }
+  return ans;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  if(num===0)return 1;
+  
+  else{
+    for (let i=1;i<=num;i++){
+      product*=i;
+    }
+    return product;
+  }	
 };
 
 // Do not edit below this line
