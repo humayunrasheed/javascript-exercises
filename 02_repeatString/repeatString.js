48f1a81a649beaf6304fa202e52a2fc66f337301
humayunrasheed <humayun.51959@gmial.com>
2023-05-24 08:19:04 +0530

    Exercises 1 and 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(a,n ) {
+    if(n<0)return "ERROR";
+    let string="";
+     for(let i=0;i<n ;i++){
+      string += a;
+     }
+     return string;
 };
 
 // Do not edit below this line
