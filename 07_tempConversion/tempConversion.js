d6519cf02c57f825a0453bc3a9e80ef64554f25b
humayunrasheed <humayun.51959@gmial.com>
2023-05-24 19:31:35 +0530

    6 and 7 exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num= parseFloat([(num-32)*5]/9,1);
+  return Math.round(num * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num= parseFloat((num *9/5)+32,1);
+  return Math.round(num * 10) / 10
 };
 
 // Do not edit below this line
