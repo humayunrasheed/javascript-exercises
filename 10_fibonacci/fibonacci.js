7f0a26de4f6c5e74550237485b1b45277ad006f2
humayunrasheed <humayun.51959@gmial.com>
2023-05-28 16:44:38 +0530

    Fabonacci function created and tested on test cases

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0){
+        return "OOPS";
+    }
+    let fib= [];
+    fib[0]=1;
+    fib[1]=1;
+    if(num>2){
+    for(let i = 2;i<num;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+    }}
+    return fib[num-1];
 };
 
 // Do not edit below this line
